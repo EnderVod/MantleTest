4dac1cb920324c616acef7574fc72752e2b6fc8a
KnightMiner <knightminer4@gmail.com>
2025-01-05 16:16:30 -0500

    Deprecate tag helper for removal
    All redundant to NBTUtils methods

--- src/main/java/slimeknights/mantle/util/TagHelper.java
+++ src/main/java/slimeknights/mantle/util/TagHelper.java
@@ -16,11 +16,8 @@ import javax.annotation.Nullable;
 public class TagHelper {
   /* BlockPos */
 
-  /**
-   * Converts a block position to NBT
-   * @param pos Position
-   * @return NBT compound
-   */
+  /** @deprecated use {@link net.minecraft.nbt.NbtUtils#writeBlockPos(BlockPos)} */
+  @Deprecated(forRemoval = true)
   public static CompoundTag writePos(BlockPos pos) {
     CompoundTag tag = new CompoundTag();
     tag.putInt("X", pos.getX());
@@ -29,12 +26,9 @@ public class TagHelper {
     return tag;
   }
 
-  /**
-   * Reads a block position from a given tag compound
-   * @param tag Tag to read
-   * @return BlockPos, or null if invalid
-   */
+  /** @deprecated use {@link net.minecraft.nbt.NbtUtils#readBlockPos(CompoundTag)} */
   @Nullable
+  @Deprecated(forRemoval = true)
   public static BlockPos readPos(CompoundTag tag) {
     if (tag.contains("X", Tag.TAG_ANY_NUMERIC) &&tag.contains("Y", Tag.TAG_ANY_NUMERIC) && tag.contains("Z", Tag.TAG_ANY_NUMERIC)) {
       return new BlockPos(tag.getInt("X"), tag.getInt("Y"), tag.getInt("Z"));
