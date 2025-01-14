bced8ea435c8e71e2156b182b5a32904627f2474
KnightMiner <knightminer4@gmail.com>
2025-01-13 20:53:42 -0500

    Add helper to set loadable for a map with values
    Same one in any collection loadable, works with sets too

--- src/main/java/slimeknights/mantle/data/loadable/mapping/SetLoadable.java
+++ src/main/java/slimeknights/mantle/data/loadable/mapping/SetLoadable.java
@@ -4,7 +4,10 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSet.Builder;
 import slimeknights.mantle.data.loadable.Loadable;
 
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /** Loadable of set of elements */
 public class SetLoadable<T> extends CollectionLoadable<T,Set<T>,Builder<T>> {
@@ -20,5 +23,10 @@ public class SetLoadable<T> extends CollectionLoadable<T,Set<T>,Builder<T>> {
   @Override
   protected Set<T> build(Builder<T> builder) {
     return builder.build();
+  }
+
+  /** Creates a map from this collection using the given getter to find values for the map */
+  public <V> Loadable<Map<T,V>> mapWithValues(Function<T,V> valueGetter) {
+    return flatXmap(collection -> collection.stream().collect(Collectors.toUnmodifiableMap(Function.identity(), valueGetter)), Map::keySet);
   }
 }
