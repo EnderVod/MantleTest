9ce87a89988524d63cdf240d33595bca30b7b87a
KnightMiner <knightminer4@gmail.com>
2025-07-25 02:40:18 -0400

    Implement cooking recipe variants with ItemOutput results
    Also just a way nicer builder.
    This is meant for Metalborn, but may want it in Ceramics as well.
    Not sure if any other recipe types are worth giving ItemOutput support.

--- src/main/java/slimeknights/mantle/recipe/MantleRecipes.java
+++ src/main/java/slimeknights/mantle/recipe/MantleRecipes.java
@@ -6,8 +6,13 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import slimeknights.mantle.Mantle;
+import slimeknights.mantle.recipe.cooking.BlastingResultRecipe;
+import slimeknights.mantle.recipe.cooking.CampfireResultRecipe;
+import slimeknights.mantle.recipe.cooking.SmeltingResultRecipe;
+import slimeknights.mantle.recipe.cooking.SmokingResultRecipe;
 import slimeknights.mantle.recipe.crafting.ShapedFallbackRecipe;
 import slimeknights.mantle.recipe.crafting.ShapedRetexturedRecipe;
+import slimeknights.mantle.recipe.helper.LoadableRecipeSerializer;
 
 /** Handles any custom recipes added by Mantle */
 public class MantleRecipes {
@@ -23,4 +28,9 @@ public class MantleRecipes {
   // crafting
   public static final RegistryObject<ShapedFallbackRecipe.Serializer> CRAFTING_SHAPED_FALLBACK = RECIPES.register("crafting_shaped_fallback", ShapedFallbackRecipe.Serializer::new);
   public static final RegistryObject<ShapedRetexturedRecipe.Serializer> CRAFTING_SHAPED_RETEXTURED = RECIPES.register("crafting_shaped_retextured", ShapedRetexturedRecipe.Serializer::new);
+  // cooking
+  public static final RegistryObject<RecipeSerializer<SmeltingResultRecipe>> SMELTING = RECIPES.register("smelting", () -> LoadableRecipeSerializer.of(SmeltingResultRecipe.LOADABLE));
+  public static final RegistryObject<RecipeSerializer<BlastingResultRecipe>> BLASTING = RECIPES.register("blasting", () -> LoadableRecipeSerializer.of(BlastingResultRecipe.LOADABLE));
+  public static final RegistryObject<RecipeSerializer<SmokingResultRecipe>> SMOKING = RECIPES.register("smoking", () -> LoadableRecipeSerializer.of(SmokingResultRecipe.LOADABLE));
+  public static final RegistryObject<RecipeSerializer<CampfireResultRecipe>> CAMPFIRE = RECIPES.register("campfire", () -> LoadableRecipeSerializer.of(CampfireResultRecipe.LOADABLE));
 }
