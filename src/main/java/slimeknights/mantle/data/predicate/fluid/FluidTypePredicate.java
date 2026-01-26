187d138f7d7857850019deccef37cc41d0957f5a
KnightMiner <knightminer4@gmail.com>
2026-01-25 22:19:26 -0500

    Added datagen helper for serializing a fluid type predicate
    Using immutable set to ensure ordering

--- src/main/java/slimeknights/mantle/data/predicate/fluid/FluidTypePredicate.java
+++ src/main/java/slimeknights/mantle/data/predicate/fluid/FluidTypePredicate.java
@@ -1,5 +1,6 @@
 package slimeknights.mantle.data.predicate.fluid;
 
+import com.google.common.collect.ImmutableSet;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.fluids.FluidType;
 import slimeknights.mantle.data.loadable.Loadables;
@@ -11,6 +12,10 @@ import java.util.Set;
 /** Predicate that checks if the fluid type matches any of a set of fluid types */
 public record FluidTypePredicate(Set<FluidType> types) implements FluidPredicate {
   public static final RecordLoadable<FluidTypePredicate> LOADER = RecordLoadable.create(Loadables.FLUID_TYPE.set().requiredField("fluid_types", FluidTypePredicate::types), FluidTypePredicate::new);
+
+  public FluidTypePredicate(FluidType... types) {
+    this(ImmutableSet.copyOf(types));
+  }
 
   @Override
   public boolean matches(Fluid fluid) {
